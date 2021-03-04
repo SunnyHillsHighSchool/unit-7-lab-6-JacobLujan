243ae6c4edd1f24d13c28290b2279b908f5be646
Jacob Lujan <600022570@fjuhsd.org>
2021-03-04 23:00:09 +0000

    Changed Word.java

--- Word.java
+++ Word.java
@@ -1,6 +1,6 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
-//Name -
+//Name - Jacob Lujan
 
 import static java.lang.System.*;
 
@@ -10,12 +10,45 @@ public class Word
    																//why is it static?
    
    //add an instance variable
-
+   private String word;
 	//create a constructor
-	
+	public Word()
+  {
+    word = "";
+  }
+  public Word(String W)
+  {
+    word = W;
+  }
+
 	//create other methods
+
+  public void setWord(String wordy)
+  {
+    word = wordy;
+  }
+
+  public int getNumVowels()
+  {
+    int numVowels = 0;
+    for(int i = 0; i < word.length(); i++)
+    {
+      if(vowels.indexOf(word.charAt(i))>= 0)
+      numVowels++;
+    }
+
+    return numVowels;
+  } 
+  public int getLength()
+  {
+    return word.length();
+  }
 	//look at the sample runner code
 	//on the lab handout
 
 	//create a toString method
+  public String toString()
+  {
+    return "" + word;
+  }
 }
